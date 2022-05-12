0b529dc71d4ff43762b29d992295615806ea90e3
Vitaliy-Pavlinchuk <vitaliy.pavlinchuk@gmail.com>
2022-05-12 13:29:57 +0300

    finish-1-task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('[data-start]');
+const btnStoP = document.querySelector('[data-stop]');
+
+
+let timeIntervalId = 0;
+
+const onChangeColor = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+};
+
+const onClickStart = () => {
+  timeIntervalId = setInterval(onChangeColor, 1000);
+  btnStart.setAttribute('disabled', true);
+};
+
+const onClickStop = () => {
+  clearInterval(timeIntervalId);
+  btnStart.removeAttribute('disabled');
+};
+
+btnStart.addEventListener('click', onClickStart);
+btnStoP.addEventListener('click', onClickStop);
